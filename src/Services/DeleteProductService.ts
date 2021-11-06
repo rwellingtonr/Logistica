25680c08e68509a523216b58b99093122b523682
rwellingtonr <wellington-158@hotmail.com>
2021-11-06 17:06:08 -0300

    Delete records and return the item deleted

--- src/Services/DeleteProductService.ts
+++ src/Services/DeleteProductService.ts
@@ -4,6 +4,7 @@ class DeleteProductService {
   async execute(item: string) {
     const itemDeleted = await prismaClient.produto.delete({
       where: { item },
+      select: { item: true },
     })
 
     return itemDeleted
